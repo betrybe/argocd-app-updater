bfb69364fff314acd11e8007f63922931e9b951d
Euclécio Josias Rodrigues <euclecio.dev@gmail.com>
2023-03-07 17:32:36 -0300

    Cria groups para melhorar log da action

--- update_params.sh
+++ update_params.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e
 
+app_name="$REPOSITORY-$ENVIRONMENT"
+secrets=$1
+
+echo "======= ArgoCD Updater ======="
+echo ""
+echo "...Aguardando o ArgoCD sincronizar a aplicação $app_name"
+echo "Para mais detalhes (logs, debug) sobre o status do deploy acesse https://deploy.betrybe.com"
+echo ""
+echo "======= ArgoCD Updater ======="
+
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/$ARGOCD_VERSION/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
-
-app_name="$REPOSITORY-$ENVIRONMENT"
-secrets=$1
 
 if [[ -z "$secrets" ]]; then
   secrets=$(env | awk -F = '/^SECRET_/ {print $1}')
@@ -20,19 +27,20 @@ do
 done
 
 if [[ -z "$secrets_list" ]]; then
+  echo "::group::Alterando secrets/envvars"
   argocd app set $app_name $secrets_list --grpc-web
+  echo "::endgroup::"
 fi
 
-if [[ -z "$IMAGE_TAG" ]] && {
+if [[ ! -z "$IMAGE_TAG" ]]; then
+  echo "::group::Alterando image.tag"
   echo "Updating image to tag: $IMAGE_TAG"
   argocd app set $app_name --helm-set image.tag=$IMAGE_TAG --grpc-web
-}
+  echo "::endgroup::"
+fi
 
-echo "======= ArgoCD Updater ======="
-echo ""
-echo "...Aguardando o ArgoCD sincronizar a aplicação"
-echo "Para mais detalhes (logs, debug) sobre o status do deploy acesse https://deploy.betrybe.com"
-echo ""
-echo "======= ArgoCD Updater ======="
-
+echo "::group::Sincronizando"
 argocd app wait $app_name --grpc-web
+echo ""
+echo "Para mais detalhes (logs, debug) sobre o status do deploy acesse https://deploy.betrybe.com"
+echo "::endgroup::"
